fa2da89ca1816f21fdeacb1d4a24bd396492d11a
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-06-16 16:11:56 +0800

    Update train.py

--- train.py
+++ train.py
@@ -363,8 +363,8 @@ def main(args=None):
         raise ValueError('When you have no validation data, you should not specify --compute-val-loss.')
 
     # start training
-    return model.fit_generator(
-        generator=train_generator,
+    return model.fit(
+        x=train_generator,
         steps_per_epoch=args.steps,
         initial_epoch=0,
         epochs=args.epochs,
