b1381e02bbebc3f3ccd3fcc00b9e5f802f0b799b
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-06-17 11:56:22 +0800

    Trying channels_first

--- model.py
+++ model.py
@@ -428,6 +428,7 @@ def efficientdet(phi, num_classes=20, num_anchors=9, weighted_bifpn=False, freez
     d_head = d_heads[phi]
     backbone_cls = backbones[phi]
     features = backbone_cls(input_tensor=np.moveaxis(image_input, -1, 0), freeze_bn=freeze_bn)
+    features = np.moveaxis(features, 0, -1)
     if weighted_bifpn:
         fpn_features = features
         for i in range(d_bifpn):
